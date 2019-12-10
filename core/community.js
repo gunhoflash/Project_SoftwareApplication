21423d8f3b3fd88c7bcbee069f3b72f04a49536e
Heluit <wbjung2917@gmail.com>
2019-12-10 18:47:58 +0900

    louvain finally uncompleted

--- core/community.js
+++ core/community.js
@@ -155,7 +155,7 @@ exports.LinkCommunity =() =>{
 
 // semi only
 // refer to https://www.isislab.it/cordasco/papers/BASNA10/BASNA_pres.pdf from https://www.nature.com/articles/srep30750
-exports.labelPropagation = (numberOfNodes, _nodeSizeNeighborArray) => {
+exports.labelPropagation = (numberOfNodes, _nodeSizeNeighborArray, iteration_limit) => {
 
 	let i, j, k;
 	let nodeSizeNeighborArray = ARRAY.clone_deep(_nodeSizeNeighborArray);
@@ -164,6 +164,8 @@ exports.labelPropagation = (numberOfNodes, _nodeSizeNeighborArray) => {
 	let color_groups = []; // colors[i] is the array of nodes that has color-i
 	
 	let communites = []; // communites[i] is the community-ID of node-i
+	
+	let counter=0; // variable for counting # of iteration
 
 	/*
 		coloring with bfs
@@ -241,7 +243,7 @@ exports.labelPropagation = (numberOfNodes, _nodeSizeNeighborArray) => {
 	for (i = 0; i < numberOfNodes; i++)
 	_numberOfCommunities[i] = 0;
 	console.log(`start LPA`);
-	while(1) {
+	while(counter<iteration_limit) { //break when can't get result in (iteration_limit) iterations 
 		//console.log(`\n -- LPA --`);
 		let convergence = true;
 		// for all color groups
@@ -285,6 +287,8 @@ exports.labelPropagation = (numberOfNodes, _nodeSizeNeighborArray) => {
 
 		// check state converged
 		if (convergence) break;
+		// add counter for not found case
+		counter++;
 	}
 
 	// make graph from communities
@@ -328,11 +332,12 @@ exports.Louvain =(graph) =>{
 	var i=0, j=0, k=0, counter=0;
 	var randomized_node_list=[];
 	var node_list=[];
+	var edge_list=[];
 	var community_list=[];
 	var community_edge_list=[];
 	var is_in_array_result;
 
-	for(i=0; i<num_of_nodes; i++)
+	for(i=0; i<num_of_nodes; i++) //phase 1의 진행 순서를 randomize하기위한 randomized_node_list생성
 	{
 		randomized_node_list[i]=Math.floor(Math.random()*(num_of_nodes));
 		for(j=0; j<i; j++)
@@ -345,13 +350,19 @@ exports.Louvain =(graph) =>{
 		}
 	}
 	
+	/*
 	console.log(randomized_node_list);
 	console.log(graph.edges);
 	console.log(num_of_neighbors);
+	*/
 
 	for(i=0; i<num_of_nodes; i++)//making node list
 	{		
 		node_list[randomized_node_list[i]]={
+			/*
+				NID : node ID(number)
+				CID : community ID that this node place
+			*/
 			NID : randomized_node_list[i],
 			CID : i
 		}
@@ -359,6 +370,10 @@ exports.Louvain =(graph) =>{
 	for(i=0; i<num_of_edges; i++)//making edge list
 	{		
 		edge_list[i]={
+			/*
+				edge : array of 2 elements like [NID,NID]
+				weight : default is 1 because we only handle undirected graph
+			*/
 			edge : edges[i],
 			weight : 1
 		}
@@ -366,16 +381,22 @@ exports.Louvain =(graph) =>{
 	for(i=0; i<num_of_nodes; i++)//making community list
 	{
 		community_list[i]={
+			/*
+			nodes : array of NIDs which is in this community
+			inner_weight : sum of weights which is absolutly in the community, default is 0 because there is just one nod in community at the first time
+			total_weight : sum of (node size) in the community
+			neighborCommunities : array of CIDs which is a neighbor of this array
+			*/
 			nodes : [node_list[randomized_node_list[i]]],
 			inner_weight : 0,
 			total_weight : num_of_neighbors[randomized_node_list[i]],
-			neighborCommunities : []
+			neighborCommunities : []	
 		}
 	}
 	for(i=0; i<num_of_edges; i++)
 	{
-		temp_community_edge=[node_list[edges[i][0]].CID,node_list[edges[i][1]].CID];
-		is_in_array_result=isInEdgeArray(community_edge_list,temp_community_edge)
+		temp_community_edge=[node_list[edges[i][0]].CID,node_list[edges[i][1]].CID]; //tempoorary save edge
+		is_in_array_result=this.isInEdgeArray(community_edge_list,temp_community_edge);
 		if(is_in_array_result[0])
 		{
 			community_edge_list[is_in_array_result[1]].weight+=1;
@@ -385,10 +406,11 @@ exports.Louvain =(graph) =>{
 			community_edge_list.push({
 				edge : temp_community_edge,
 				weight : 1
-			})
+			});
 		}
 	}
-
+	
+	/*
 	for(i=0;i<num_of_nodes;i++)
 	{
 		console.log("node"+i);
@@ -413,31 +435,31 @@ exports.Louvain =(graph) =>{
 		console.log("total_weight : "+community_list[i].total_weight);
 		console.log("neighborCommunities : "+community_list[i].neighborCommunities+"\n");
 	}
-
+	*/
 	
-	/*
+	
 	var num_of_change=0;
 	
 	while(true)
 	{
 		//phase1
-		this.louvainPhase1(node_list,community_list, edge_list, num_of_change);
+		//this.louvainPhase1(node_list,community_list, edge_list, num_of_change);
 		if(num_of_change==0)
 		{
 			break;
 		}
 		//phase2
-		this.louvainPhase2(community_list, edge_list, num_of_change);
+		//this.louvainPhase2(community_list, edge_list, num_of_change);
 	}
-	*/
 };
+
 exports.isInEdgeArray=(community_edge_list,temp_community_edge)=>{
 	var counter=0;
 	var i=0;
 	var where=-1;
 	for(i=0; i<community_edge_list.length; i++)
 	{
-		if((coummunity_edge_list[i].edge[0]==temp_community_edge[0])&&(coummunity_edge_list[i].edge[1]==temp_community_edge[1]))
+		if((community_edge_list[i].edge[0]==temp_community_edge[0])&&(community_edge_list[i].edge[1]==temp_community_edge[1]))
 		{
 			counter++;
 			where=i;
@@ -452,10 +474,9 @@ exports.isInEdgeArray=(community_edge_list,temp_community_edge)=>{
 	{
 		return [true, where];
 	}
-}
+};
 
 exports.louvainPhase1 =(node_list,community_list, edge_list, num_of_change) =>{
-	/*
 	var nlist=node_list;
 	var clist=community_list;
 	var elist=edge_list;
@@ -467,33 +488,11 @@ exports.louvainPhase1 =(node_list,community_list, edge_list, num_of_change) =>{
 	{
 		for(j=0; j<clist[i].neighborCommunities.length; j++)
 		{
-			delta_M=( ((clist[i].inner_weight+1)/sum_of_weight) - Math.pow((clist[i].tot_weight+),2))-((clist[i].inner_weight/sum_of_weight)-Math.pow(,2)-Math.pow(,2));
-			console.log(i+"	"+clist[i].neighborCommunities[j]);
+			//delta_M=( ((clist[i].inner_weight+1)/sum_of_weight) - Math.pow((clist[i].tot_weight+),2))-((clist[i].inner_weight/sum_of_weight)-Math.pow(,2)-Math.pow(,2));
+			//console.log(i+"	"+clist[i].neighborCommunities[j]);
 		}
 	}
-	*/
-}
+};
 
 exports.louvainPhase2 =(community_list, edge_list, num_of_change) =>{
-}
-
-//dumpstation-------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	/*
-		
-	*/
-	
-	/*
-	for(i=0; i<num_of_nodes; i++)
-	{
-		console.log(neighbors[i]);
-	}
-	for(i=0; i<num_of_nodes; i++)
-	{
-		console.log(node_list[i].node_num+"	"+node_list[i].CID);
-		console.log(community_list[i].nodes+"	"+community_list[i].neighborCommunities+"\n");
-	}	
-	for(i=0; i<edge_list.length; i++)
-	{
-		console.log(edge_list[i].edge+"	"+edge_list[i].weight);
-	}
-	*/
+};
